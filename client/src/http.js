2d4d8013ef14dd37db21a5892181285479b2dd80
shuzootani <suzo.yolo@gmail.com>
2017-12-19 23:15:58 +0900

    base url for production

--- client/src/http.js
+++ client/src/http.js
@@ -1,10 +1,4 @@
-const BASE_URL = () => {
-  if (process.env.NODE_ENV === "production") {
-    return "https://yumelog.herokuapp.com"  
-  } else {
-    return "http://localhost:3000"
-  }
-}
+const BASE_URL = 'https://yumelog.herokuapp.com'
 
 const csrfToken = document.getElementsByName("csrf-token").item(0).content;
 const same_origin = {
@@ -48,29 +42,29 @@ const to_json = body => {
 };
 
 export const get = path => {
-  return fetch(BASE_URL() + path).then(res => res.json());
+  return fetch(BASE_URL + path).then(res => res.json());
 };
 
 export const get_with_auth = path => {
-  return fetch(BASE_URL() + path, { ...get_header }).then(res => res.json());
+  return fetch(BASE_URL + path, { ...get_header }).then(res => res.json());
 };
 
 export const post = (path, params = {}) => {
   let body = to_json(params);
-  return fetch(BASE_URL() + path, { ...post_header, body }).then(res =>
+  return fetch(BASE_URL + path, { ...post_header, body }).then(res =>
     res.json()
   );
 };
 
 export const patch = (path, params = {}) => {
   let body = to_json(params);
-  return fetch(BASE_URL() + path, { ...patch_header, body }).then(res =>
+  return fetch(BASE_URL + path, { ...patch_header, body }).then(res =>
     res.json()
   );
 };
 
 export const destroy = path => {
-  return fetch(BASE_URL() + path, { ...delete_header }).then(res =>
+  return fetch(BASE_URL + path, { ...delete_header }).then(res =>
     res.json()
   );
 };
